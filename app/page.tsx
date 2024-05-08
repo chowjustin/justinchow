76071ad229a744d6150e963b1564de85573c3028
chowjustin <justin.chow05@gmail.com>
2024-05-09 02:44:10 +0700

    style: change favicon

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,25 @@ import Achievements from "./components/Achievements";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="max-w-screen overflow-hidden">
-      <Header />
-      <HomeLayout />
-      <About />
-      <Education />
-      <Achievements />
-      <Skills />
-      <Contact />
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="mask-icon" href="/favicon.ico" color="#000000" />
+      </Head>
+      <main className="max-w-screen overflow-hidden">
+        <Header />
+        <HomeLayout />
+        <About />
+        <Education />
+        <Achievements />
+        <Skills />
+        <Contact />
+        <Footer />
+      </main>
+    </>
   );
 }
