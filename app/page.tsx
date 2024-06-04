c2548608b74d261499be50cff142e550e1da7148
chowjustin <justin.chow05@gmail.com>
2024-06-04 12:36:51 +0700

    feat: change favicon

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,31 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="mask-icon" href="/favicon.ico" color="#000000" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/public/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/public/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/public/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <main className="max-w-screen overflow-x-hidden">
         <Header />
